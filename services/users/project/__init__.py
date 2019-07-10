01c24f225236fca18164a76ff8cb789dc7e3f4c0
Marcus Rands <marcus.rands@scholarpack.com>
2019-07-10 15:19:51 +0100

    flask course progress up to Blueprints.

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,21 +1,31 @@
-from flask import Flask, jsonify
-from flask_restful import Resource, Api
+import os
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 
-# instantiate the app
-app = Flask(__name__)
+# instantiate the db
+db = SQLAlchemy()
 
-api = Api(app)
+def create_app(script_info=None):
 
-# set config
-app.config.from_object('project.config.DevelopmentConfig')
+    # instantiate the app
+    app = Flask(__name__)
 
-class UsersPing(Resource):
-    def get(self):
-        return {
-        'status': 'success',
-        'message': 'pong!'
-   	 }
+    # set config
+    app_settings = os.getenv('APP_SETTINGS')
+    app.config.from_object(app_settings)
 
+    # set up extensions
+    db.init_app(app)
 
-api.add_resource(UsersPing, '/users/ping')
+    # register blueprints
+    from project.api.users import users_blueprint
+    app.register_blueprint(users_blueprint)
+
+    # shell context for flask cli
+    @app.shell_context_processor
+    def ctx():
+        return {'app': app, 'db': db}
+
+    return app
